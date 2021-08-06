f444fa409c318f7ae49be07982bf23a061c507bd
Dusty <mrgeek484@gmail.com>
2021-08-06 14:42:50 +0800

    first

---
+++ commit.js
@@ -0,0 +1,40 @@
+const { exec } = require('child_process');
+
+(async () => {
+	// add files
+	await exec('git add .', (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+	// commit
+	await exec('git commit -am commit', (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+	// push
+	await exec('git push origin --all', (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+})();
